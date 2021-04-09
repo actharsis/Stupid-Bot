ef458a36f51dddbc073f25758c2dea2ce2c13360
actharsis <actharsis@gmail.com>
2021-04-09 21:19:42 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+settings = {
+    'token': 'Your bot token',
+    'bot': 'Bot name',
+    'id': 123456789 #bot id
+}
---
+++ main.py
@@ -0,0 +1,48 @@
+import os, random
+import discord
+from discord.ext import commands
+from config import settings
+from datetime import datetime
+
+client = discord.Client()
+
+start_time = datetime.now()
+start_time.isoformat(sep='T')
+memory = ''
+count = 0
+cache_users = []
+
+@client.event
+async def on_message(message):
+    if message.author.id == client.user.id:
+        return
+        
+    if message.content.startswith('$StartTime'):
+        await message.channel.send('Bot started working at ' + str(start_time.strftime("%b %d %Y %H:%M:%S")))
+    elif message.content.startswith('$Homoquote'):
+        random_file_name = random.choice(os.listdir(os.getcwd() + "/img/homoquotes"))
+        await message.channel.send(file=discord.File("img/homoquotes/" + random_file_name))
+
+    await message_repeating(message)
+    return
+
+async def message_repeating(message):
+    global count
+    global memory
+    if count == 0:
+        cache_users.append(message.author.id)
+        memory = message.content
+        count += 1
+    elif message.content == memory:
+        if count == 2:
+            await message.channel.send(memory)
+            memory = ""
+        elif not message.author.id in cache_users:
+            cache_users.append(message.author.id)
+            count += 1
+    else:
+        memory = ""
+        memory = message.content
+        count = 0
+
+client.run(settings['token'])
