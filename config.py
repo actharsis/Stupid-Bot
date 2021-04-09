19d01f6a59ca871a245f5df2eecbeda44f96753b
YAPONE4 <lxprin@gmail.com>
2021-04-09 22:02:19 +0300

    Fix commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODMwMDkxMTU3OTA3OTYzOTQ2.YHBoog.x75riFGgABc_30KQIryssH31ATg',
-    'bot': 'TabaBot',
-    'id': 830091157907963946
+    'token': 'Your bot token',
+    'bot': 'Botname',
+    'id': 12345678 # Your bot ID
 }
