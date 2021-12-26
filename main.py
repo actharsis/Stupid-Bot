98611b59f80443f195a6f6b99deda24389c1c074
vladyslav-yeremenko <vladyslav.yeremenko@globallogic.com>
2021-12-26 15:41:06 +0200

    added replies

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import discord
 import constants
+import discord
 import os, random
 from modules.message_analysis import Analysis_module
 from discord.ext import commands
@@ -9,16 +9,86 @@ from datetime import datetime
 import yt_dlp
 import asyncio
 
-client = commands.Bot(command_prefix=settings['prefix'], case_insensitive=True, help_command=None)
-analyzer = Analysis_module(client)
+# misc init
 start_time = datetime.now()
 start_time.isoformat(sep='T')
+true_random = random.SystemRandom()
 history = { }
-very_clever_quotes = []
 
 with open(constants.CLEVER_QUOTES_DIR, encoding='utf-8') as file:
         very_clever_quotes = file.read().split(";")
 
+replies = None
+with open('replies.txt', encoding="utf-8") as f:
+    lines = f.read().splitlines()
+if len(lines) > 0:
+    pairs = [l.split('//')[0].split('->') for l in lines]
+    for p in pairs:
+        p[1] = p[1].split(";")
+    replies = {int(p[0]):p[1] for p in pairs}
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}   
+
+
+# functions
+def endSong(guild, path):
+    os.remove(path)
+
+
+def get_special_replies(author_id):
+    if author_id in replies:
+        return replies[author_id]
+    else:
+        return []
+
+
+async def random_vot_da(ctx):
+    if ctx.author.id == client.user.id:
+        return
+    if random.random() < 0.01:
+        await ctx.channel.send('вот да')
+    elif random.random() < 0.005:
+        await ctx.channel.send(random.choice(very_clever_quotes))
+
+
+async def message_repeating(ctx):
+    if ctx.channel.id in history and ctx.content != '':
+        if history[ctx.channel.id]['text'] == ctx.content:
+            history[ctx.channel.id]['count'] += 1
+            if(history[ctx.channel.id]['count'] == constants.MESSAGES_TO_REPEAT):
+                await ctx.channel.send(history[ctx.channel.id]['text'])
+                history[ctx.channel.id]['text'] = ''
+                history[ctx.channel.id]['count'] = 0
+        else:
+            history[ctx.channel.id]['text'] = ctx.content
+            history[ctx.channel.id]['count'] = 1
+    else:
+        history[ctx.channel.id] = {'text': ctx.content, 'count': 1}
+
+async def reference_reaction(ctx):
+    if ctx.reference and ctx.reference.resolved.author.id == client.user.id and ctx.author.id != client.user.id:
+
+        if replies:
+            special_replies = get_special_replies(ctx.author.id)
+            if special_replies:
+                special_reply = true_random.choice(special_replies)
+                reply = f"{ctx.author.mention}, {special_reply}"
+            else:
+                reply = ctx.channel.send(f"{ctx.author.mention}, вы кто?")
+        await ctx.channel.send(reply)
+
+# client init
+client = commands.Bot(command_prefix=settings['prefix'], case_insensitive=True, help_command=None)
+analyzer = Analysis_module(client)
+
+# commands
 @client.event
 async def on_message(ctx):
     if ctx.author.id == client.user.id:
@@ -26,13 +96,10 @@ async def on_message(ctx):
 
     await random_vot_da(ctx)
     await message_repeating(ctx)
+    await reference_reaction(ctx)
     analyzer.save_message(ctx)
 
     await client.process_commands(ctx)
-
-
-def endSong(guild, path):
-    os.remove(path)
 
 
 @client.command(name='disconnect', pass_ctx=True)
@@ -104,27 +171,5 @@ async def top(ctx):
         await analyzer.get_voice_activity(ctx)
 
 
-async def random_vot_da(ctx):
-    if ctx.author.id == client.user.id:
-        return
-    if random.random() < 0.01:
-        await ctx.channel.send('вот да')
-    elif random.random() < 0.005:
-        await ctx.channel.send(random.choice(very_clever_quotes))
-
-
-async def message_repeating(ctx):
-    if ctx.channel.id in history and ctx.content != '':
-        if history[ctx.channel.id]['text'] == ctx.content:
-            history[ctx.channel.id]['count'] += 1
-            if(history[ctx.channel.id]['count'] == constants.MESSAGES_TO_REPEAT):
-                await ctx.channel.send(history[ctx.channel.id]['text'])
-                history[ctx.channel.id]['text'] = ''
-                history[ctx.channel.id]['count'] = 0
-        else:
-            history[ctx.channel.id]['text'] = ctx.content
-            history[ctx.channel.id]['count'] = 1
-    else:
-        history[ctx.channel.id] = {'text': ctx.content, 'count': 1}
-
+# exec
 client.run(settings['token'], bot = True)
