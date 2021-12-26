5a039849a1c4190eb418d91194a7bb19989cde45
actharsis <actharsis@gmail.com>
2021-12-26 14:41:31 +0300

    added A LOT of shit code

--- TODO.md
+++ TODO.md
@@ -6,8 +6,12 @@
 > - Most usable words per user
 > - Graph of user points history (after voice activity fix preferable)
 > - Most active time for single user
-> - Import all message to DB
+> - Import all messages to DB
 > - Fix shitty code in message analysis module
+> - Make tests
+> - DB querries optimization
 
 BUGS:
-> - When bot join server commands Voice and Top returns ```
+> - When bot join server commands Voice and Top returns ```. It caused by no rows in tables for this server.
+> - Songs with ' crashing
+> - Voice disconnect after playing song
--- main.py
+++ main.py
@@ -5,22 +5,80 @@ from modules.message_analysis import Analysis_module
 from discord.ext import commands
 from config import settings
 from datetime import datetime
+import yt_dlp
+import sys
 
 client = commands.Bot(command_prefix=settings['prefix'], case_insensitive=True, help_command=None)
 analyzer = Analysis_module(client)
 start_time = datetime.now()
 start_time.isoformat(sep='T')
 history = { }
+very_clever_quotes = []
+
+with open('clever_quotes.txt', encoding='utf-8') as file:
+        very_clever_quotes = file.read().split(";")
 
 @client.event
 async def on_message(ctx):
     if ctx.author.id == client.user.id:
         return
 
+    await random_vot_da(ctx)
     await message_repeating(ctx)
     analyzer.save_message(ctx)
 
     await client.process_commands(ctx)
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}   
+
+def endSong(guild, path):
+    os.remove(path)
+
+
+@client.command(name='disconnect', pass_ctx=True)
+async def disconnect(ctx):
+    vc = ctx.message.guild.voice_client
+    await vc.disconnect()
+
+
+@client.command(pass_context=True)
+async def play(ctx, url):
+    if not ctx.message.author.voice:
+        await ctx.send('you are not connected to a voice channel')
+        return
+
+    else:
+        channel = ctx.message.author.voice.channel
+
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    if voice is None:
+        voice_client = await channel.connect()
+
+    guild = ctx.message.guild
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        file = ydl.extract_info(url, download=True)
+        path = str(file['title']) + " [" + str(file['id'] + "].mp3")
+
+    voice_client.play(discord.FFmpegPCMAudio(path), after=lambda x: endSong(guild, path))
+    voice_client.source = discord.PCMVolumeTransformer(voice_client.source, 1)
+
+    await ctx.send(f'**Music: **{url}')
+
+    #while voice_client.is_playing():
+    #    await asyncio.sleep(1)
+    #else:
+    #    await voice_client.disconnect()
+    #    print("Disconnected")
 
 
 @client.command(name='RenaStare')
@@ -53,6 +111,15 @@ async def top(ctx):
 @client.command(name='Voice')
 async def top(ctx):
         await analyzer.get_voice_activity(ctx)
+
+
+async def random_vot_da(ctx):
+    if ctx.author.id == client.user.id:
+        return
+    if random.random() < 0.01:
+        await ctx.channel.send('вот да')
+    elif random.random() < 0.005:
+        await ctx.channel.send(random.choice(very_clever_quotes))
 
 
 async def message_repeating(ctx):
