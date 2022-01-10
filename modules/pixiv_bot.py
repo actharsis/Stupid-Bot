7ba4845b2e680ef378a6a7a243d90163a30689cd
BdE <iwansychew@gmail.com>
2022-01-10 10:12:49 +0500

    async Pixiv bot implemented, multichannel support added, refactor

--- modules/pixiv_bot.py
+++ modules/pixiv_bot.py
@@ -1,12 +1,13 @@
 import asyncio
+import base64
 import datetime
 import emoji
 import json
 import os
+import random
 import time
 
 from PIL import Image
-from constants import PIXIV_AUTO_QUERY_DELAY
 from discord import Embed, File
 from discord.colour import Colour
 from discord.ext import commands
@@ -27,6 +28,21 @@ def is_date(date_text):
         return False
 
 
+def str_time_prop(start, end, time_format, prop):
+    stime = time.mktime(time.strptime(start, time_format))
+    etime = time.mktime(time.strptime(end, time_format))
+    ptime = stime + prop * (etime - stime)
+    return time.strftime(time_format, time.localtime(ptime))
+
+
+def random_date(start, end, prop):
+    return str_time_prop(start, end, '%Y-%m-%d', prop)
+
+
+def current_date():
+    return time.strftime('%Y-%m-%d', time.localtime(time.time()))
+
+
 def read_pixiv_refresh_token():
     with open("pixiv_token.txt", "r") as f:
         token = f.readline()
@@ -36,9 +52,6 @@ def read_pixiv_refresh_token():
 class BetterAppPixivAPI(AppPixivAPI):
     def download(self, url, prefix='', path=os.path.curdir, name=None, replace=False, fname=None,
                  referer='https://app-api.pixiv.net/'):
-        """Download image to file (use 6.0 app-api)"""
-        file = name or os.path.basename(url)
-
         with self.requests_call('GET', url, headers={'Referer': referer}, stream=True) as response:
             return Image.open(response.raw)
 
@@ -49,36 +62,57 @@ class PixivCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.chat = None
-        self.last_auto_update = None
+        self.channels = {}
+        self.timers = {}
         self.last_query = None
         self.last_type = None
         self.token_expiration_time = None
+        self.load()
 
-    def check_picture_channel(self, ctx):
-        ch = ctx.channel
-        return ch == self.chat
+    def save(self, channels=False, timers=False):
+        if channels:
+            with open('auto_pixiv_channels.json', 'w') as file:
+                file.write(json.dumps(self.channels))
+        if timers:
+            with open('auto_pixiv_timers.json', 'w') as file:
+                file.write(json.dumps(self.timers))
 
-    async def show_page(self, query, limit=None):
-        if self.chat is not None:
-            if query.illusts is None:
-                return False
-            for index, illust in enumerate(query.illusts):
-                if limit is not None and index == limit:
-                    break
-                filename = str(illust.id) + '.png'
-                title = illust.title
-                with BytesIO() as image_binary:
-                    img = self.api.download(illust.image_urls.large)
-                    img.save(image_binary, 'PNG')
-                    image_binary.seek(0)
-                    await self.chat.send('Title: ' + title, file=File(fp=image_binary, filename=filename))
-            return True
-        return False
+    def load(self):
+        try:
+            with open('auto_pixiv_channels.json', 'r') as file:
+                self.channels = json.load(file)
+            with open('auto_pixiv_timers.json', 'r') as file:
+                self.timers = json.load(file)
+        except:
+            pass
 
-    async def show_page_embed(self, query, query_type, limit=None, save_query=True):
-        success = await self.show_page(query, limit)
-        if success:
+    async def show_page(self, query, channel, limit=30, minimum_views=None, minimum_rate=None):
+        if channel is None or query.illusts is None:
+            return 0, False
+        shown = 0
+        print('fetched', len(query.illusts), 'images')
+        for illust in query.illusts:
+            if shown == limit:
+                break
+            if minimum_views is not None and illust.total_view < minimum_views:
+                continue
+            if minimum_rate is not None and illust.total_bookmarks / illust.total_view * 100 < minimum_rate:
+                continue
+            filename = str(illust.id) + '.png'
+            title = illust.title
+            with BytesIO() as image_binary:
+                img = self.api.download(illust.image_urls.large)
+                img.save(image_binary, 'PNG')
+                image_binary.seek(0)
+                message = await channel.send('Title: ' + title, file=File(fp=image_binary, filename=filename))
+                shown += 1
+            if illust.is_bookmarked:
+                await message.add_reaction(emoji.emojize(':red_heart:'))
+        return shown, True
+
+    async def show_page_embed(self, query, query_type, chat, limit=None, save_query=True):
+        shown, result = await self.show_page(query, chat, limit)
+        if result:
             if save_query:
                 self.last_query = query
                 self.last_type = query_type
@@ -86,11 +120,42 @@ class PixivCog(commands.Cog):
         else:
             return Embed(title="Pixiv API can't process the request", color=Colour.red())
 
-    @cog_ext.cog_slash(name="start", description="Select current channel as default for pixiv illustrations")
-    async def start_pixiv(self, ctx):
-        self.chat = ctx.channel
-        embed = Embed(title="Art channel selected", color=Colour.green())
+    @cog_ext.cog_slash(name="start_auto_pixiv", description="Add channel to the auto Pixiv list",
+                       options=[
+                           create_option(
+                               name="refresh_time",
+                               description="Delay between auto update in minutes (default = 180)",
+                               option_type=SlashCommandOptionType.INTEGER,
+                               required=False,
+                           ),
+                           create_option(
+                               name="limit",
+                               description="Amount of pictures will be displayed (default = 20)",
+                               option_type=SlashCommandOptionType.INTEGER,
+                               required=False,
+                           )])
+    async def add_auto_pixiv(self, ctx, refresh_time=180, limit=20):
+        channel_id = str(ctx.channel.id)
+        if channel_id in self.channels.keys():
+            embed = Embed(title="Auto Pixiv already running on this channel", color=Colour.gold())
+        else:
+            self.channels[channel_id] = {"refresh_time": refresh_time * 60, "limit": limit}
+            embed = Embed(title="Auto Pixiv is now running on this channel", color=Colour.green())
         await ctx.send(embed=embed)
+        self.save(channels=True)
+
+    @cog_ext.cog_slash(name="stop_auto_pixiv", description="Delete channel from the auto Pixiv list")
+    async def delete_auto_pixiv(self, ctx):
+        channel_id = str(ctx.channel.id)
+        if channel_id in self.channels.keys():
+            self.channels.pop(channel_id)
+            if channel_id in self.timers.keys():
+                self.timers.pop(channel_id)
+            embed = Embed(title="Channel has been deleted", color=Colour.green())
+        else:
+            embed = Embed(title="This channel not exist in the list", color=Colour.gold())
+        await ctx.send(embed=embed)
+        self.save(channels=True, timers=True)
 
     @cog_ext.cog_slash(name='next', description="Show next page from the last 'best' query",
                        options=[
@@ -102,19 +167,19 @@ class PixivCog(commands.Cog):
                            )])
     async def next(self, ctx, limit=10):
         await ctx.defer()
-        if self.check_picture_channel(ctx) and self.last_query is not None:
+        if self.last_query is not None:
             next_qs = self.api.parse_qs(self.last_query.next_url)
             query = None
             if self.last_type == 'recommended':
                 query = self.api.illust_recommended(**next_qs)
             elif self.last_type == 'best':
-                query = self.api.illust_recommended(**next_qs)
-            embed = await self.show_page_embed(query, self.last_type, limit, save_query=True)
+                query = self.api.illust_ranking(**next_qs)
+            embed = await self.show_page_embed(query, self.last_type, ctx.channel, limit, save_query=True)
         else:
-            embed = Embed(title="This channel is not defined as illustration channel", color=Colour.red())
+            embed = Embed(title="Previous request not found", color=Colour.red())
         await ctx.send(embed=embed, delete_after=5.0)
 
-    @cog_ext.cog_slash(name='recommended', description="Show [10] recommended pixiv illustrations",
+    @cog_ext.cog_slash(name='recommended', description="Show [10] recommended Pixiv illustrations",
                        options=[
                            create_option(
                                name="limit",
@@ -124,11 +189,8 @@ class PixivCog(commands.Cog):
                            )])
     async def recommended(self, ctx, limit=10):
         await ctx.defer()
-        if self.check_picture_channel(ctx):
-            query = self.api.illust_recommended()
-            embed = await self.show_page_embed(query, 'recommended', limit, save_query=True)
-        else:
-            embed = Embed(title="This channel is not defined as illustration channel", color=Colour.red())
+        query = self.api.illust_recommended()
+        embed = await self.show_page_embed(query, 'recommended', ctx.channel, limit, save_query=True)
         await ctx.send(embed=embed, delete_after=5.0)
 
     @cog_ext.cog_slash(name='best', description='Find best [10] rated illutstrations with specific mode and date',
@@ -164,20 +226,44 @@ class PixivCog(commands.Cog):
                        ])
     async def best(self, ctx, mode='month', limit=10, date=None):
         await ctx.defer()
-        if self.check_picture_channel(ctx):
-            query = self.api.illust_ranking(mode=mode, date=date, offset=None)
-            embed = await self.show_page_embed(query, 'best', limit, save_query=True)
-        else:
-            embed = Embed(title="This channel is not defined as illustration channel", color=Colour.red())
+        query = self.api.illust_ranking(mode=mode, date=date, offset=None)
+        embed = await self.show_page_embed(query, 'best', ctx.channel, limit, save_query=True)
         await ctx.send(embed=embed, delete_after=5.0)
 
     @cog_ext.cog_slash(name='when', description='Show remaining time until new illustrations')
     async def time_to_update(self, ctx):
-        if self.last_auto_update is None:
-            embed = Embed(title="timer has not started", color=Colour.gold())
+        channel_id = str(ctx.channel.id)
+        if channel_id in self.timers.keys() and channel_id in self.channels:
+            refresh_time = self.channels[channel_id]['refresh_time']
+            last_update_time = self.timers[channel_id]
+            delta = int(refresh_time + last_update_time - time.time())
+            embed = Embed(title=str(delta // 60) + ' min ' + str(delta % 60) + ' secs until autopost',
+                          color=Colour.green())
         else:
-            delta = str(int(PIXIV_AUTO_QUERY_DELAY + self.last_auto_update - time.time()))
-            embed = Embed(title=str(delta) + ' secs remain before autopost', color=Colour.green())
+            embed = Embed(title="Timer has not started", color=Colour.gold())
+        await ctx.send(embed=embed, delete_after=10.0)
+
+    @cog_ext.cog_slash(name='secret', description='???')
+    async def test_feature(self, ctx):
+        await ctx.defer()
+        if ctx.author_id == 360641399713955843:
+            limit = 5
+            min_views = 4000
+            min_rate = 15.0
+            date = random_date('2017-01-01', current_date(), random.random())
+            counter = 0
+            offset = 0
+            alive = True
+            while counter < limit and alive:
+                query = self.api.search_illust(base64.b64decode('6KqV55Sf5pel').decode("utf-8"),
+                                               sort='date_asc', start_date=date, offset=offset)
+                shown, alive = await self.show_page(query, ctx.channel, limit, min_views, min_rate)
+                counter += shown
+                if alive:
+                    offset += len(query.illusts)
+            embed = Embed(title="Secret feature called", color=Colour.green())
+        else:
+            embed = Embed(title="You can't use this query", color=Colour.red())
         await ctx.send(embed=embed, delete_after=10.0)
 
     @commands.Cog.listener()
@@ -201,11 +287,11 @@ class PixivCog(commands.Cog):
                     self.api.illust_bookmark_add(illust_id)
                     await reaction.message.add_reaction(emoji.emojize(':red_heart:'))
                     query = self.api.illust_related(illust_id)
-                    await self.show_page(query, limit=5)
+                    await self.show_page(query, reaction.message.channel, limit=5)
                 elif demojized == ':magnifying_glass_tilted_left:':
                     await reaction.message.add_reaction(emoji.emojize(':thumbs_up:'))
                     query = self.api.illust_related(illust_id)
-                    await self.show_page(query, limit=5)
+                    await self.show_page(query, reaction.message.channel, limit=5)
                 elif demojized == ':broken_heart:':
                     self.api.illust_bookmark_delete(illust_id)
                     for r in reaction.message.reactions:
@@ -226,12 +312,21 @@ class PixivCog(commands.Cog):
                 await reaction.remove(user)
 
     async def auto_draw(self):
-        if self.chat is not None:
-            timestamp = time.time()
-            if self.last_auto_update is None or timestamp - self.last_auto_update > PIXIV_AUTO_QUERY_DELAY:
-                query = self.api.illust_recommended()
-                await self.show_page(query, limit=20)
-                self.last_auto_update = timestamp
+        try:
+            for channel_id, options in self.channels.items():
+                channel = self.bot.get_channel(int(channel_id))
+                if channel is None:
+                    continue
+                refresh_time = options['refresh_time']
+                limit = options['limit']
+                timestamp = time.time()
+                if channel_id not in self.timers.keys() or timestamp - self.timers[channel_id] > refresh_time:
+                    query = self.api.illust_recommended()
+                    await self.show_page(query, channel, limit=limit)
+                    self.timers[str(channel_id)] = timestamp
+                    self.save(timers=True)
+        except RuntimeError:
+            pass
 
     async def auto_refresh_token(self):
         timestamp = time.time()
