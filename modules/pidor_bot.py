8e55748fcf722efad2146a0338c1f0c741a51450
BdE <iwansychew@gmail.com>
2022-01-12 22:57:19 +0500

    json parse fixed

--- modules/pidor_bot.py
+++ modules/pidor_bot.py
@@ -34,11 +34,12 @@ class PidorCog(commands.Cog):
     async def roll(self, ctx):
         await ctx.defer()
         cur_time = str(current_date())
-        if ctx.guild.id not in self.pidor_channels.keys():
-            self.pidor_channels[ctx.guild.id] = {'time': cur_time, 'pidor': None}
+        channel_id = str(ctx.guild.id)
+        if channel_id not in self.pidor_channels.keys():
+            self.pidor_channels[channel_id] = {'time': cur_time, 'pidor': None}
 
-        if self.pidor_channels[ctx.guild.id]['time'] != cur_time or \
-                self.pidor_channels[ctx.guild.id]['pidor'] is None:
+        if self.pidor_channels[channel_id]['time'] != cur_time or \
+                self.pidor_channels[channel_id]['pidor'] is None:
             await ctx.send(embed=Embed(title="Rolling cubes to decide Pidor Of The Day",
                                        color=Colour.gold()), delete_after=15.0)
             await asyncio.sleep(3)
@@ -48,13 +49,13 @@ class PidorCog(commands.Cog):
                 await asyncio.sleep(1)
             idx = random.randint(0, len(ctx.guild.members) - 1)
             user = str(ctx.guild.members[idx])
-            self.pidor_channels[ctx.guild.id]['pidor'] = user
-            self.pidor_channels[ctx.guild.id]['time'] = cur_time
+            self.pidor_channels[channel_id]['pidor'] = user
+            self.pidor_channels[channel_id]['time'] = cur_time
             embed = Embed(title="Pidor of the day: " + user, color=Colour.gold())
             self.save()
         else:
             embed = Embed(title="Pidor of the day has already been chosen. Pidor: " +
-                                self.pidor_channels[ctx.guild.id]['pidor'], color=Colour.gold())
+                                self.pidor_channels[channel_id]['pidor'], color=Colour.gold())
         await ctx.send(embed=embed, delete_after=180.0)
 
 
